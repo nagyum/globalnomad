2552db7eb17dc8e05e1f9c18a3d5e81093be74b3
sybusay <sy_busay@naver.com>
2025-03-19 13:56:53 +0900

    [#46] feat/공통컴포넌트 인풋 (#48)
    * feat: 공통컴포넌트 인풋 작업 완료
    
    * feat: 공통컴포넌트 인풋에 대한 스토리북 작업 완료
    
    * feat: disabled가 true일 때 라벨 배경색 수정
    
    * chore: 코드리뷰 반영 - 오타 수정, label 스타일 수정, className 정리
    
    * chore: 코드리뷰 반영 - 오타 수정

---
+++ src/components/Input.tsx
@@ -0,0 +1,83 @@
+import { cva, type VariantProps } from 'class-variance-authority';
+import { cn } from '@/lib/cn';
+import { useState } from 'react';
+
+const InputVariants = cva(
+  'border border-gray-800 bg-white w-full text-black-100 placeholder-gray-500 rounded-lg py-4 px-5 font-normal text-base leading-[26px]',
+  {
+    variants: {
+      variant: {
+        default: '',
+        event: '',
+      },
+      disabled: {
+        true: 'bg-gray-200 text-gray-600',
+      },
+    },
+
+    defaultVariants: {
+      variant: 'default',
+      disabled: false,
+    },
+  },
+);
+
+interface InputProps extends VariantProps<typeof InputVariants> {
+  id?: string;
+  label?: string;
+  type?: string;
+  placeholder?: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  className?: string;
+  value: string;
+  disabled?: boolean;
+}
+
+export default function Input({
+  variant,
+  disabled = false,
+  id,
+  label,
+  type = 'text',
+  placeholder = '',
+  onChange,
+  className,
+  value,
+}: InputProps) {
+  const isEvent = variant === 'event';
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleFocus = () => setIsFocused(true);
+  const handleBlur = () => setIsFocused(value !== '');
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!disabled) onChange(e);
+  };
+
+  return (
+    <div className='relative flex flex-col gap-[8px]'>
+      <label
+        htmlFor={id}
+        className={cn(
+          'text-black-100 inline-block bg-clip-text text-base leading-[26px] font-normal',
+          isEvent &&
+            'absolute top-1/2 z-10 -translate-y-1/2 transform px-5 text-gray-500 transition-all duration-200 ease-in-out',
+          (isFocused || value) && isEvent && 'top-1/2 -translate-y-10 transform text-[14px]',
+          className,
+        )}
+      >
+        <span className={cn('cursor-pointer px-[8px]', disabled && isEvent ? 'bg-gray-200' : 'bg-white')}>{label}</span>
+      </label>
+      <input
+        id={id}
+        type={type}
+        onChange={handleChange}
+        placeholder={isEvent ? '' : placeholder}
+        disabled={disabled}
+        className={cn(InputVariants({ variant, disabled }), className)}
+        value={value}
+        onFocus={isEvent ? handleFocus : undefined}
+        onBlur={isEvent ? handleBlur : undefined}
+      />
+    </div>
+  );
+}
---
+++ src/stories/Input.stories.tsx
@@ -0,0 +1,120 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Pretendard } from '@/font';
+import Input from '@/components/Input';
+import { action } from '@storybook/addon-actions';
+import { useState } from 'react';
+
+const meta: Meta<typeof Input> = {
+  title: 'Input',
+  component: Input,
+  tags: ['autodocs'],
+  argTypes: {
+    variant: { control: 'radio', options: ['default', 'event'] },
+    type: { control: 'radio', options: ['email', 'password', 'text'] },
+    onChange: { action: 'changed' },
+    disabled: { control: 'boolean' },
+  },
+  decorators: [
+    (Story) => (
+      <div className={Pretendard.className}>
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Input>;
+
+export const Default: Story = {
+  args: {
+    variant: 'default',
+    id: 'nickname',
+    label: '닉네임',
+    placeholder: '닉네임을 입력해 주세요',
+    type: 'text',
+    onChange: action('changed'),
+  },
+};
+
+export const Event: Story = {
+  args: {
+    variant: 'event',
+    id: 'activities',
+    label: '내가 원하는 체험은',
+    type: 'text',
+    className: 'pl-[48px]',
+
+    onChange: action('changed'),
+  },
+  render: (args) => {
+    const ActivitiesSearchInput = () => {
+      const [value, setValue] = useState('');
+
+      return (
+        <Input
+          {...args}
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+            args.onChange(e);
+          }}
+        />
+      );
+    };
+
+    return <ActivitiesSearchInput />;
+  },
+};
+
+export const PasswordType: Story = {
+  args: {
+    variant: 'default',
+    id: 'newPassword',
+    label: '새 비밀번호',
+    placeholder: '새 비밀번호를 한번 더 입력해주세요',
+    type: 'newPassword',
+    onChange: action('changed'),
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    variant: 'default',
+    label: '이메일',
+    value: 'test@email.com',
+    type: 'email',
+    disabled: true,
+    onChange: action('changed'),
+  },
+};
+
+export const Custom: Story = {
+  args: {
+    variant: 'event',
+    id: 'email',
+    label: '이메일',
+    type: 'email',
+    className: 'w-[400px] rounded px-7',
+    onChange: action('changed'),
+  },
+  render: (args) => {
+    const ActivitiesSearchInput = () => {
+      const [value, setValue] = useState('');
+
+      return (
+        <Input
+          {...args}
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+            args.onChange(e);
+          }}
+        />
+      );
+    };
+
+    return <ActivitiesSearchInput />;
+  },
+};
